b174b96e02c337b2f4351b9b5a2b5a0bb91eed07
Jaehoon Lee <wannabewize@gmail.com>
2023-10-28 16:44:36 +0900

    폴더 이름 변경

--- 09.ListAndApp/ListEditingExample_SwiftUI/ListEditingExample/ListEditingExample_App.swift
+++ 09.ListAndApp/ListEditingExample_SwiftUI/ListEditingExample/ListEditingExample_App.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct ListEditingExample_SwiftUIApp: App {
+struct ListEditingExample_App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()

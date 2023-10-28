41cf224145581ad4ce64ef4129e03225bc038d35
Jaehoon Lee <wannabewize@gmail.com>
2023-10-28 19:16:16 +0900

    UIKit 기반 App Navigation 예제 [WIP]

---
+++ 11.AppNavigation/ListAndDetailApp_UIKit/ListAndDetailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListAndDetailApp
+//
+//  Created by wannabewize on 10/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

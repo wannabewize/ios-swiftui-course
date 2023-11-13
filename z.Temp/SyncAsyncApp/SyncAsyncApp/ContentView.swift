18c8871d9504284a95af098a7c2eb87a9a0f7e76
Jaehoon Lee <wannabewize@gmail.com>
2023-11-14 01:46:08 +0900

    다운로드 코드 분리

--- z.Temp/SyncAsyncApp/SyncAsyncApp/ContentView.swift
+++ z.Temp/SyncAsyncApp/SyncAsyncApp/ContentView.swift
@@ -7,65 +7,13 @@
 
 import SwiftUI
 
-// 4mb image
-let urlStr = "https://upload.wikimedia.org/wikipedia/commons/f/f4/The_Scream.jpg"
-
-
 struct ContentView: View {
     @State var image: UIImage?
     @State var value: Float = 0.5
     
-    // 동기식
-    func syncDownloadImage() -> UIImage? {
-        let url = URL(string: urlStr)!
-        let data = try! Data(contentsOf: url)
-        print("Download Done")
-                
-        image = UIImage(data: data)
-        return image
-    }
-    
-    // Grand Central Dispatch를 이용한 비동기
-    func gcdDownloadImage(_ completion: @escaping (UIImage?) -> Void) {
-        DispatchQueue.global().async {
-            let url = URL(string: urlStr)!
-            let data = try! Data(contentsOf: url)
-            print("Download Done", Thread.isMainThread)
-            
-            let image = UIImage(data: data)
-            completion(image)
-        }
-    }
-    
-    
-    // URLSession 기반의 비동기 네트워크
-    func sessionBasedTask(_ completion: @escaping (UIImage?) -> Void) {
-        let url = URL(string: urlStr)!
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            print("Download Done", Thread.isMainThread)
+    // 4mb image
+    let urlStr = "https://upload.wikimedia.org/wikipedia/commons/f/f4/The_Scream.jpg"
 
-            if let data = data,
-               let image = UIImage(data: data) {
-                completion(image)
-            }
-        }
-        task.resume()
-    }
-    
-    // Swift Concurrency를 활용한 비동기 네트워크
-    func awaitBasedTask() async -> UIImage? {
-        let url = URL(string: urlStr)!
-        let ret = try? await URLSession.shared.data(from: url)
-        print("Download Done")
-        
-        if let data = ret?.0 {
-            let image = UIImage(data: data)
-            return image
-        }
-        
-        return nil
-    }
     
     var body: some View {
         VStack(spacing: 20) {
@@ -75,20 +23,20 @@ struct ContentView: View {
             HStack(spacing: 20) {
                 Button("Sync") {
                     image = nil
-                    let downloaded = syncDownloadImage()
+                    let downloaded = syncDownloadImage(urlStr)
                     image = downloaded
                 }
                 
                 Button("GCD") {
                     image = nil
-                    gcdDownloadImage { downloaded in
+                    gcdDownloadImage(urlStr) { downloaded in
                         self.image = downloaded
                     }
                 }
                 
                 Button("URLSession") {
                     image = nil
-                    sessionBasedTask { downloaded in
+                    sessionBasedTask(urlStr) { downloaded in
                         self.image = downloaded
                     }
                 }
@@ -96,7 +44,7 @@ struct ContentView: View {
                 Button("AsyncAwait") {
                     image = nil
                     Task {
-                        let downloaded = await awaitBasedTask()
+                        let downloaded = await awaitBasedTask(urlStr)
                         self.image = downloaded
                     }
                 }
---
+++ z.Temp/SyncAsyncApp/SyncAsyncApp/ImageDownloadService.swift
@@ -0,0 +1,61 @@
+//
+//  ImageDownloadService.swift
+//  SyncAsyncApp
+//
+//  Created by wannabewize on 11/14/23.
+//
+
+import Foundation
+import UIKit
+
+// 동기식
+func syncDownloadImage(_ urlStr: String) -> UIImage? {
+    let url = URL(string: urlStr)!
+    let data = try! Data(contentsOf: url)
+    print("Download Done")
+            
+    let image = UIImage(data: data)
+    return image
+}
+
+// Grand Central Dispatch를 이용한 비동기
+func gcdDownloadImage(_ urlStr: String, completion: @escaping (UIImage?) -> Void) {
+    DispatchQueue.global().async {
+        let url = URL(string: urlStr)!
+        let data = try! Data(contentsOf: url)
+        print("Download Done", Thread.isMainThread)
+        
+        let image = UIImage(data: data)
+        completion(image)
+    }
+}
+
+
+// URLSession 기반의 비동기 네트워크
+func sessionBasedTask(_ urlStr: String, completion: @escaping (UIImage?) -> Void) {
+    let url = URL(string: urlStr)!
+    let request = URLRequest(url: url)
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        print("Download Done", Thread.isMainThread)
+
+        if let data = data,
+           let image = UIImage(data: data) {
+            completion(image)
+        }
+    }
+    task.resume()
+}
+
+// Swift Concurrency를 활용한 비동기 네트워크
+func awaitBasedTask(_ urlStr: String) async -> UIImage? {
+    let url = URL(string: urlStr)!
+    let ret = try? await URLSession.shared.data(from: url)
+    print("Download Done")
+    
+    if let data = ret?.0 {
+        let image = UIImage(data: data)
+        return image
+    }
+    
+    return nil
+}

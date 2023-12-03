ce367fb7a15dcbecb21b8d594dcd73b2386334f6
Jaehoon Lee <wannabewize@gmail.com>
2023-12-04 01:35:42 +0900

    User Defaults 로 저장하기 예제

---
+++ 18.Persistence/UserDefault/UserDefault/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  UserDefault
+//
+//  Created by wannabewize on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var title: String = "알람 - 123"
+    @State var date: Date = Date()
+    @State var notify: Bool = false
+    @State var volume: Double = 0.5
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            HStack(spacing: 20) {
+                Spacer()
+                Button("읽기") {
+                    let setting = UserDefaults()
+
+                    if let title = setting.string(forKey: "TITLE"),
+                        let date = setting.value(forKey: "DATE") as? Date,
+                       let notify = setting.value(forKey: "NOTIFY") as? Bool,
+                       let volume = setting.value(forKey: "VOLUME") as? Double {
+                        
+                        self.title = title
+                        self.date = date
+                        self.notify = notify
+                        self.volume = volume
+                    }
+                    else {
+                        print("Something wrong")
+                    }
+                }
+                
+                Button("저장") {
+                    let setting = UserDefaults()
+                    setting.setValue(date, forKey: "DATE")
+                    setting.setValue(notify, forKey: "NOTIFY")
+                    setting.setValue(volume, forKey: "VOLUME")
+                    setting.setValue(title, forKey: "TITLE")
+                    
+                    print("Success")
+                }
+            }
+            
+            HStack(spacing: 30) {
+                Text("이름")
+                TextField("이름", text: $title)
+            }
+            
+            DatePicker("Date", selection: $date)
+                .pickerStyle(.inline)
+                
+            Toggle("미리 알림", isOn: $notify)
+            
+            HStack(spacing: 30) {
+                Text("음량(\(String(format: "%0.1f", volume)))")
+                
+                Slider(value: $volume, in: 0.0...1.0) {
+                    Text("\(volume)")
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 18.Persistence/UserDefault/UserDefault/UserDefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultApp.swift
+//  UserDefault
+//
+//  Created by wannabewize on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

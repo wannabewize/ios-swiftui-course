932818f41b01cd1dbd705f4e3e84fde45ddd21b8
Jaehoon Lee <wannabewize@limefriends.com>
2024-01-17 19:48:01 +0900

    ING

--- z.Temp/VariousShapes/VariousShapes/ContentView.swift
+++ z.Temp/VariousShapes/VariousShapes/ContentView.swift
@@ -24,7 +24,46 @@ struct ContentView: View {
             
             Text("Border")
                 .frame(width: 100, height: 100)
-                .border(Color.black, width: 1)
+                .background(.blue)
+                .border(Color.black, width: 3)
+            
+            Text("Rounded Shape")
+                .frame(width: 100, height: 100)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                )
+            
+            Text("Rounded Border")
+                .frame(width: 100, height: 100)
+                .background(.blue)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 10)
+                )
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 1.0)
+                }
+            
+            Text("Hidden")
+                .frame(width: 100, height: 100)
+                .background(.blue)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.yellow)
+                        .padding(5)
+                }
+                .overlay {
+                    Circle()
+                        .fill(.red)
+                        .padding(10)
+                }
+                .overlay {
+                    Text("Overlays")
+                        .font(.headline)
+                }
+            
+            
             
             Text("Shadow")
                 .frame(width: 100, height: 100)

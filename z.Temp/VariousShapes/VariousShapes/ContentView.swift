c80fc7a10d2749ef9e156fc3a1f9b8e8049b5c63
Jaehoon Lee <wannabewize@gmail.com>
2024-01-18 18:23:00 +0900

    코너 방향 예제 추가

--- z.Temp/VariousShapes/VariousShapes/ContentView.swift
+++ z.Temp/VariousShapes/VariousShapes/ContentView.swift
@@ -9,33 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Circle by Clip")
-                .frame(width: 100, height: 100)
-                .background(.blue)
-                .clipShape(Circle())
-                .foregroundStyle(.white)
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("Circle by Clip")
+                    .frame(width: 100, height: 100)
+                    .background(.blue)
+                    .clipShape(Circle())
+                    .foregroundStyle(.white)
 
-            Text("Rounded Rectange")
-                .frame(width: 100, height: 100)
-                .background(.blue)
-                .clipShape(
-                    RoundedRectangle(cornerSize: CGSize(width: 20, height: 10), style: .continuous), style: FillStyle())
-            
-            Text("Border")
-                .frame(width: 100, height: 100)
-                .border(Color.black, width: 1)
-            
-            Text("Shadow")
-                .frame(width: 100, height: 100)
-                .background(.blue)
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            
-            
-
-            
+                Text("Rounded Rectange")
+                    .frame(width: 100, height: 100)
+                    .background(.blue)
+                    .clipShape(
+                        RoundedRectangle(cornerSize: CGSize(width: 20, height: 10), style: .continuous), style: FillStyle())
+                
+                Text("Border")
+                    .frame(width: 100, height: 100)
+                    .border(Color.black, width: 1)
+                
+                Text("Shadow")
+                    .frame(width: 100, height: 100)
+                    .background(.blue)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                
+                Text("Uneven Rounded")
+                    .frame(width: 100, height: 100)
+                    .background(.blue)
+                    .clipShape(
+                        UnevenRoundedRectangle(topLeadingRadius: 10, bottomLeadingRadius: 10)
+                    )
+                
+                Text("Uneven Rounded Border")
+                    .frame(width: 100, height: 100)
+                    .overlay {
+                        UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(bottomTrailing: 10, topTrailing: 10), style: .continuous)
+                            .stroke(lineWidth: 1)
+                    }
+            }
         }
-        .padding()
     }
 }
 

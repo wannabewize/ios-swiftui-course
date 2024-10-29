b7a5f2732e8415b5eec7077b8c1b6b00a7d4e5c2
Jaehoon Lee <wannabewize@gmail.com>
2024-10-30 02:19:14 +0900

    리스트 쉬운 예제 추가

---
+++ 10.ListAndGrid/BasicListExamples/BasicListExamples/BasicListExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicListExamplesApp.swift
+//  BasicListExamples
+//
+//  Created by wannabewize on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicListExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 10.ListAndGrid/BasicListExamples/BasicListExamples/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  BasicListExamples
+//
+//  Created by wannabewize on 10/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+            Text("List and Multi-Views").font(.subheadline)
+            List {
+                Text("Item - 1")
+                Text("Item - 2")
+                Text("Item - 3")
+                Text("Item - 3")
+                Text("Item - 4")
+            }
+            Divider()
+            
+            Text("List and Collection/Range").font(.subheadline)
+            List(0..<5) { item in
+                Text("Item - \(item)")
+            }
+
+            Divider()
+
+            Text("List and ForEach").font(.subheadline)
+            List {
+                ForEach(0..<5) { item in
+                    Text("Item - \(item)")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

63459f0404ce21f0cca94ec1b5945a96926bc19b
wannabewize <wannabewize@gmail.com>
2024-11-04 20:59:03 +0900

    다이나믹 리스트 예제 작성

--- 10.ListAndGrid/DynamicListExample/DynamicListExample/ContentView.swift
+++ 10.ListAndGrid/DynamicListExample/DynamicListExample/ContentView.swift
@@ -7,17 +7,79 @@
 
 import SwiftUI
 
+// For Keypath
+struct InnerItem {
+    let innerId: Int
+}
+
+struct WrapperItem {
+    let inner: InnerItem
+}
+
+struct CustomItem {
+    let id: Int
+    let title: String
+}
+
+struct CustomItem2: Identifiable {
+    let id: Int
+    let title: String
+}
+
+struct CustomItem3: Identifiable {
+    let id = UUID()
+    let title: String
+}
+
 struct ContentView: View {
     let textData = ["item a", "item b", "item c", "item d"]
     let numericData = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    let customData = [
+        CustomItem(id: 1, title: "일"),
+        CustomItem(id: 2, title: "이"),
+        CustomItem(id: 3, title: "삼"),
+        CustomItem(id: 4, title: "사"),
+        CustomItem(id: 5, title: "오"),
+    ]
+    let customData2 = [
+        CustomItem2(id: 1, title: "하나"),
+        CustomItem2(id: 2, title: "둘"),
+        CustomItem2(id: 3, title: "셋"),
+        CustomItem2(id: 4, title: "넷"),
+        CustomItem2(id: 5, title: "다섯"),
+    ]
+    
+    let customData3 = [
+        CustomItem3(title: "one"),
+        CustomItem3(title: "two"),
+        CustomItem3(title: "three"),
+        CustomItem3(title: "four"),
+        CustomItem3(title: "five"),
+    ]
 
     var body: some View {
         VStack {
             List(textData, id: \.self) { item in
                 Text(item)
             }
-            List(numericData, id: \.self) { item in
-                Text("Item \(item)")
+            List() {
+                ForEach(numericData, id: \.self) { item in
+                    Text("Item \(item)")
+                }
+            }
+            
+            List(customData, id: \.id) { item in
+                Text("Item \(item.title)")
+            }
+            
+            List(customData2) { item in
+                Text("Item \(item.title)")
+            }
+            
+            List {
+                ForEach(customData3) { item in
+                    Text("Item \(item.title)")
+                }
             }
         }
         .padding()
--- 10.ListAndGrid/README.md
+++ 10.ListAndGrid/README.md
@@ -4,6 +4,10 @@
 
 SwiftUI의 List를 이용한 정적 목록(Static List) 작성 예제
 
+## Dynamic List Example
+
+콜렉션(Array)를 이용해서 런타임시 목록 내 콘텐츠가 결정되는 리스트 예제.
+
 ## List and Custom Type
 
 커스텀 타입의 모델 데이터를 리스트로 출력하기, 리스트 내 셀 터치 이벤트

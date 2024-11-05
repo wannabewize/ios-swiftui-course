ba2e425d7a58a6ab61108683d6fd434933685809
Jaehoon Lee <wannabewize@gmail.com>
2024-11-06 02:34:41 +0900

    내비게이션 바 커스터마이징 예제 이름 변경

---
+++ 11.AppNavigation/TabBarCustomization/TabBarCustomization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabBarCustomization
+//
+//  Created by wannabewize on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 11.AppNavigation/TabBarCustomization/TabBarCustomization/TabBarCustomizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarCustomizationApp.swift
+//  TabBarCustomization
+//
+//  Created by wannabewize on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TabBarCustomizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
